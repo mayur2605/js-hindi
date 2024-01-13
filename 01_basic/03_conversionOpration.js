b968dfc008b427505a185e9f99ef660b141d769a
mayur2605 <57018500+mayur2605@users.noreply.github.com>
2024-01-13 11:49:49 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionOpration.js
@@ -0,0 +1,29 @@
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN
+//true => 1; false => 0
+
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1 => true; 0 => false
+//"" => false //empty string
+//"data in string" = true
+
+let someNumber = 33
+let stringNumber = someNumber
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
