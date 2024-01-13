ba5feb684c6e800f60135b27c8c4b3b3d638831e
mayur2605 <57018500+mayur2605@users.noreply.github.com>
2024-01-13 11:31:32 +0000

    data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // trat alll js code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 +3);
+console.log("Mayur");
+
+let name = "mayur"
+let age = 20
+let isLoggedIn = true
+
+//number => 2 to power of 53
+//bigint
+//string => ""
+//boolean => true / false
+//null => standalone value type,
+//undefined => when value is not defined
+//symol => finding uniqueness
+//object 
+
+
+
+console.log(typeof Symbol);
