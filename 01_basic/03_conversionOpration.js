4fa4f7e292418e6506f56c9ce56eeb7bc0d52529
mayur2605 <57018500+mayur2605@users.noreply.github.com>
2024-01-13 12:07:34 +0000

    opration

--- 01_basic/03_conversionOpration.js
+++ 01_basic/03_conversionOpration.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = 0
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 //1 => true; 0 => false
@@ -25,5 +25,50 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 let stringNumber = someNumber
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+//********************************************opration****************************************** 
+
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = "mayur"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log((3 + 8 ) * 5 % 7);
+
+// console.log(+true);
+// console.log(++"");
+
+let num1, num2, num3
+num1 = num2 =  num3 = 2 +2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
