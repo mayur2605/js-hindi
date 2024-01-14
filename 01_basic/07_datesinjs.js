908316050a56a1af2cdbad618b4fba8b2d16c899
mayur2605 <57018500+mayur2605@users.noreply.github.com>
2024-01-14 11:40:49 +0000

    datein js

---
+++ 01_basic/07_datesinjs.js
@@ -0,0 +1,32 @@
+let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate = new Date("2024-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myCreatedDate.getTime());
+
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDay());
+
+
+newDate.toLocaleString('default',{
+    weekday: "long", timeZone:
+})
+
