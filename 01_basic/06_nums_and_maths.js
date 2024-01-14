05dce8902792d13ee99ce068384a81df3f0f8ac1
mayur2605 <57018500+mayur2605@users.noreply.github.com>
2024-01-14 11:19:41 +0000

    math and num

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,47 @@
+// const score = 400
+
+
+// const balance = new Number (100)
+
+// console.log(balance);
+// console.log(score);
+
+
+
+// console.log(balance.toFixed(2));
+
+
+// const otherNumber = 21.97584
+
+// console.log(otherNumber.toPrecision(2));
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++++++++++++++++Math++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(44.2));
+// console.log(Math.pow());
+
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log((Math.random()*10) + 1);
+
+
+const min = 20
+const max = 50
+
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
+
+
+
+
+
