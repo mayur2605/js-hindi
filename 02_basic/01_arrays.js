b204fb7f1e5c22fe86a4a3aa286e941d0c821b7d
mayur2605 <57018500+mayur2605@users.noreply.github.com>
2024-01-14 12:09:24 +0000

    arrays

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,37 @@
+// //array
+
+const myArry = [0, 1, 2, 3, 4, 5,]
+
+// const myHeros = ["shaktiman", "nagraj"]
+
+// // console.log(myArry[1]);
+
+// // myArry.push(6)
+// // myArry.push(7)
+// // myArry.pop()
+
+// // myArry.unshift(9)
+// // myArry.shift()
+// // console.log(myArry.includes(5));
+
+// const newArry = myArry.join()
+
+// console.log(myArry);
+// console.log(newArry);
+
+
+//  slice, splice
+
+console.log("A", myArry);
+
+const myn1 = myArry.slice(0, 4)
+
+console.log(myn1);
+console.log("B", myArry);
+
+const myn2 = myArry.splice(0, 4)
+console.log(myn1);
+console.log(myn2);
+
+
+
