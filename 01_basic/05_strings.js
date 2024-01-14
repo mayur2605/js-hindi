462855f4809b1ec9a21c5ec794bf98ba53c12210
mayur2605 <57018500+mayur2605@users.noreply.github.com>
2024-01-14 09:35:14 +0000

    strings

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,41 @@
+const name = "mayur"
+const repoCount = 50
+
+// console.log(name + repoCount);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('mayur-k')
+
+// console.log(gameName.__proto__);
+
+
+
+
+// console.log(gameName.toLocaleLowerCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('r'));
+
+// const newString = gameName.substring(0, 3)
+
+// console.log(newString);
+
+
+// const anotherString = gameName.slice(-8, 4)
+
+
+// console.log(anotherString);
+
+
+
+
+// const newStringOne = "     Mayur     "
+
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://mayur.com?mayur%20kulkarni"
+
+console.log(url.replace("%20", '_'));
+
+console.log(url.includes('mayur'))
